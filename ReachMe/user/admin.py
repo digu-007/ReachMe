806b56f6c0e3674bcd85a52375344bcdf4c5e265
digu-007 <jasonryder555@gmail.com>
2020-11-26 19:24:20 +0530

    Add Interest model

--- ReachMe/user/admin.py
+++ ReachMe/user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import UserInfo
+from .models import *
 
 
 admin.site.register(UserInfo)
+admin.site.register(Interest)
--- ReachMe/user/models.py
+++ ReachMe/user/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Interest(models.Model):
+    name = models.CharField(max_length=127, null=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class UserInfo(models.Model):
     GENDER_OPTIONS = (
                     ('Male', 'Male'),
@@ -15,8 +21,8 @@ class UserInfo(models.Model):
     profile_pic = models.ImageField(null=True, blank = True)
     date_of_birth = models.DateField(max_length=8, null=True)
     gender = models.CharField(max_length=15, null=True, blank=True, choices=GENDER_OPTIONS)
+    interests = models.ManyToManyField(Interest)
     # location =
-    # interests =
 
     def __str__(self):
         return self.name
