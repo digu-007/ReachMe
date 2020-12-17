ecf3f3cce4062190d7bbf7c5fcaeb2d175c10f67
digu-007 <jasonryder555@gmail.com>
2020-12-17 23:23:46 +0530

    Add UserInfo form

--- ReachMe/user/forms.py
+++ ReachMe/user/forms.py
@@ -3,7 +3,15 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import UserInfo
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+class CreateUserInfoForm(ModelForm):
+    class Meta:
+        model = UserInfo
+        fields = ['user', 'name', 'phone', 'gender', 'city', 'date_of_birth', 'interests']
