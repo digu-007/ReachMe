f6dc2772ccc02f65628d7c7b182dcad621fde80a
Chirag Thakur <chirag11032000@gmail.com>
2020-12-19 20:29:40 +0530

    Updates login, register and settings page

--- ReachMe/user/forms.py
+++ ReachMe/user/forms.py
@@ -8,10 +8,79 @@ from .models import UserInfo
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = (
+            'username',
+            'email',
+            'password1',
+            'password2'
+        )
+        widgets = {
+            'username': forms.TextInput(
+                attrs = {
+                    'placeholder': 'Username'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs = {
+                    'placeholder': 'Email'
+                }
+            ),
+            'password1': forms.PasswordInput(
+                attrs = {
+                    'placeholder': 'Password'
+                }
+            )
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'})
 
 
 class CreateUserInfoForm(ModelForm):
     class Meta:
         model = UserInfo
-        fields = ['user', 'name', 'phone', 'gender', 'city', 'date_of_birth', 'interests']
+        fields = (
+            'name',
+            'phone',
+            'profile_pic',
+            'date_of_birth',
+            'gender',
+            'city',
+            'interests',
+            'bio'
+        )
+        widgets = {
+            'name': forms.TextInput(
+                attrs = {
+                    'placeholder': 'Name'
+                }
+            ),
+            'phone': forms.TextInput(
+                attrs = {
+                    'placeholder': 'Phone Number'
+                }
+            ),
+            'date_of_birth': forms.DateInput(
+                attrs = {
+                    'placeholder': 'Date of Birth'
+                }
+            ),
+            'city': forms.TextInput(
+                attrs = {
+                    'placeholder': 'City'
+                }
+            ),
+            'interests': forms.SelectMultiple(
+                attrs = {
+                    'data-placeholder': 'Choose your interests',
+                    'class': 'chosen-select'
+                }
+            ),
+            'bio': forms.TextInput(
+                attrs = {
+                    'placeholder': 'Short bio about you'
+                }
+            ),
+        }
--- ReachMe/user/views.py
+++ ReachMe/user/views.py
@@ -17,6 +17,7 @@ def homePage(request):
     }
     return render(request, 'user/home.html', context)
 
+
 def registerPage(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
@@ -36,12 +37,13 @@ def registerPage(request):
             messages.success(request, 'Account was created for ' + username)
             return redirect('settings')
 
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'user/register.html', context)
+
 
 def loginPage(request):
     if request.user.is_authenticated:
-        return redirect('dashboard')
+        return redirect('/')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -51,30 +53,33 @@ def loginPage(request):
 
         if user is not None:
             login(request, user)
-            return redirect('dashboard')
+            return redirect('/')
         else:
             messages.info(request, 'Username or password is incorrect')
 
     context = {}
     return render(request, 'user/login.html', context)
 
+
 def logoutUser(request):
     logout(request)
     return redirect('login')
 
+
 @login_required(login_url='login')
 def dashboardPage(request):
     return render(request, 'user/dashboard.html')
+
 
 @login_required(login_url='login')
 def settingsPage(request):
     form = CreateUserInfoForm(initial={'user': request.user})
 
     if request.method == 'POST':
-        form = CreateUserInfoForm(request.POST)
+        form = CreateUserInfoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('dashboard')
+            return redirect('/')
 
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'user/settings.html', context)
