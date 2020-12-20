6be414017b29d17cb833a4c2ec138c0f7bac3b21
digu-007 <jasonryder555@gmail.com>
2020-12-20 16:57:53 +0530

    Add matching algorithm

--- ReachMe/ReachMe/settings.py
+++ ReachMe/ReachMe/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'feed',
     'user',
-    'chat',
     'rest_framework'
 ]
 
